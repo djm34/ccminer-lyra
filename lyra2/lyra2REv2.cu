631571d7c2b2c919edbfc52b92c3edfbc6f15157
djm34 <djm34@aliceadsl.fr>
2015-08-09 13:59:34 +0200

    intensity on lyra2rev2 can now be adjusted with "-i" in command line

--- lyra2/lyra2REv2.cu
+++ lyra2/lyra2REv2.cu
@@ -82,9 +82,10 @@ extern "C" int scanhash_lyra2v2(int thr_id, uint32_t *pdata,
 	unsigned long *hashes_done)
 {
 	const uint32_t first_nonce = pdata[19];
-	unsigned int intensity = (device_sm[device_map[thr_id]] > 500) ? 256 * 256 * 4 : 256 * 256 * 4;//256 * 256 * 25 : 256 * 256 * 19;
-	uint32_t throughput = (device_sm[device_map[thr_id]] == 500) ? 256 * 256 * 4 : 256 * 256 * 8; // device_intensity(device_map[thr_id], __func__, intensity); // 18=256*256*4;
-	
+    unsigned int intensity = 256*256*8;
+	intensity = (device_sm[device_map[thr_id]] == 500) ? 256 * 256 * 4 : intensity;
+	uint32_t throughput = device_intensity(device_map[thr_id], __func__, intensity); 
+
 	if (opt_benchmark)
 		((uint32_t*)ptarget)[7] = 0x00ff;
 
